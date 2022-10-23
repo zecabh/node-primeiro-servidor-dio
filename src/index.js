8cbddd60ec1018814a3e258aece0ca3657d9760a
José Oliveira <zecbh@yahoo.com.br>
2022-10-23 11:21:57 -0300

    Add modo de gerenciamento de memoria

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const path = require("path")
 
+const stats = require("./pcRamUsage.js")
+
 const app = express()
 const router = express.Router() 
 
@@ -10,6 +12,10 @@ router.get("/", (req, res)=>{
 
 router.get("/contato", (req, res)=>{
     res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+router.get("/stats", (req, res)=>{
+    res.end(JSON.stringify(stats, null, 2))
 })
 
 app.use(router)
